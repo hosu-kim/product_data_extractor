52b99d6611e6fda596cea06aae59d909171f3785
hosu-kim <hosu@outlook.cz>
2025-05-19 23:02:40 +0200

    Add initial implementation of product data extractor

---
+++ product_data_extractor.ts
@@ -0,0 +1,41 @@
+/*
+file: product_data_extractor.ts
+description: 
+author: Hosu Kim with ♥️ to Apify
+created: 05/19/25 21:06:21 UTC
+*/
+
+interface Product
+{
+	[key: string]: any;
+}
+
+interface ApiResponseData
+{
+	total: number;
+	count: number;
+	products: Product[];
+}
+
+async function fetchData(url: string): Promise<ApiResponseData>
+{
+	try
+	{
+		const response = await fetch(url);
+
+		if (!response.ok)
+		{
+			throw new Error(`HTTP error. status: ${response.status}`);
+		}
+
+		const data: ApiResponseData = await response.json();
+		return data;
+
+	}
+	catch (error)
+	{
+		console.error("Error occured during the API call: ", error);
+		throw error;
+	}
+}
+
